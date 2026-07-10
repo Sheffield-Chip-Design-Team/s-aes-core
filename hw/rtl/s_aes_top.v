aa4274897ede13403b5da609f38cfbf141d1f970
Ralph Greer-Walker <gralp@Ralphs-MacBook-Pro.local>
2026-07-10 13:37:09 +0100

    define top level module

--- hw/rtl/s_aes_top.v
+++ hw/rtl/s_aes_top.v
@@ -1,9 +1,12 @@
 module s_aes_top (
-    input wire a,
-    input wire b,
-    output wire c
+    input wire clk,            // clock pulse
+    input wire rst_n,          // reset when low
+    input wire start,          // signal to start
+    input wire encrypt,        // 1 = encrypt, 0 = decrypt
+    input wire [15:0] data_in,
+    input wire [15:0] key,
+    output reg [15:0] data_out,
+    output reg done, // high when data_out is valid
 );
-    // Ralph
-    assign c = a ^ b;
 
 endmodule
