17432a8d5089c12c0457cb7ec71142f4942a0141
Ralph Greer-Walker <gralp@Ralphs-MacBook-Pro.local>
2026-07-14 03:11:23 +0100

    Fix errors in key expansion

--- hw/rtl/expand_key.v
+++ hw/rtl/expand_key.v
@@ -1,15 +1,25 @@
 
 module expand_key(
   input  wire [15:0] key_in,
-  output reg  [15:0] key_out
+  output wire [15:0] key_out
 );
 
-wire [7:0] b0 = key_in[15:8];
-wire [7:0] b1 = key_in[7:0];
+  function automatic [7:0] g;
+      input [7:0] in;
+      begin
+      end
+  endfunction
+  wire [7:0] b0; 
+  wire [7:0] b1; 
+  wire [7:0] b2; 
+  wire [7:0] b3; 
 
-wire [7:0] b2 = b0 ^ g(b1);
-wire [7:0] b3 = b2 ^ b1;
+  assign b0 = key_in[15:8];
+  assign b1 = key_in[7:0];
 
-assign key_out = {b2, b3};
+  assign b2 = b0 ^ g(b1);
+  assign b3 = b2 ^ b1;
+
+  assign key_out = {b2, b3};
 
 endmodule
