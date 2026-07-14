c5785867805f7fc0d11e8cb6eff96f6f75c68115
Ralph Greer-Walker <gralp@Ralphs-MacBook-Pro.local>
2026-07-14 02:33:24 +0100

    Clean up top file

--- hw/rtl/s_aes_top.v
+++ hw/rtl/s_aes_top.v
@@ -1,67 +1,58 @@
 `include "hw/rtl/substitute.v"
 `include "hw/rtl/shift_rows.v"
 `include "hw/rtl/mix_columns.v"
+`include "hw/rtl/expand_key.v"
 
 module s_aes_top (
     input wire clk,            // clock pulse
     input wire rst_n,          // reset when low
     input wire start,          // signal to start
-    input wire encrypt,        // 1 = encrypt, 0 = decrypt
-    input wire [15:0] data_in,
+    input wire [15:0] plaintext,
     input wire [15:0] key,
-    output reg [15:0] data_out,
+    output reg [15:0] ciphertext,
     output reg done // high when data_out is valid
 );
     // NOTE: I think if you wanted to area-optimise you can combine the shift and
     // substitution steps, will validate when full tb is set up
+    //
+    // TODO:
+    // - Do MixColumns
+    // - Do KeyExpand
     
-    // get the two extra round keys
-
-    // first AddRoundKey just uses plain key
     wire [15:0] data_xor;
     assign data_xor = data_in ^ key;
     wire [15:0] rk1, rk2;
-    expand_key (.key_in(key), .rk1(rk1), .rk2(rk2));
 
+    expand_key round_key0 (.key_in(key), .key_out(rk1));
+    expand_key round_key1 (.key_in(rk1), .key_out(rk2));
 
-    wire [15:0] sub0_result; // result of the first s-box
-    // by doing it nibble by nibble they each get their own line in the
-    // waveform, useful for validation & tb
-    substitute sbox00 (.nibble_in(data_xor[15:12]), .nibble_out(sub0_result[15:12]));
-    substitute sbox01 (.nibble_in(data_xor[11: 8]), .nibble_out(sub0_result[11: 8]));
-    substitute sbox02 (.nibble_in(data_xor[ 7: 4]), .nibble_out(sub0_result[ 7: 4]));
-    substitute sbox03 (.nibble_in(data_xor[ 3: 0]), .nibble_out(sub0_result[ 3: 0]));
-    
+    // Steps:
+    // 1.1. Substitute
+    // 1.2. ShiftRows
+    // 1.4. MixColumns
+    // 1.5. xor rk1
+    // 2.1  Substitute
+    // 2.2  ShiftRows
+    // 2.3  xor rk2
+    // return
+    wire [15:0] sub1;
+    substitute substitute_1 (.data_in(data_xor), .data_out(sub1));
+    wire [15:0] shift1;
+    shift_rows shift_1 (.data_in(sub1), .data_out(shift1));
+    wire [15:0] mix1;
+    mix_columns mix_1 (.data_in(shift1),.data_out(mix1));
+    wire [15:0] round1;
+    assign round1 = mix1 ^ rk1;
 
-    // shift rows (swap last two nibbles of each byte)
-    wire [15:0] shift0_result;
-    shift_rows shift0 (.data_in(sub0_result), .data_out(shift0_result));
-
-    wire [15:0] mix_columns0_result;
-    mix_columns mix0 (.data_in(shift0_result), .data_out(mix_columns0_result));
-
-    wire [15:0] round1_result;
-    assign round1_result = mix_columns0_result ^ rk1;
-
-
-    // round 2
-    wire [15:0] sub1_result;
-    substitute sbox10 (.nibble_in(data_xor[15:12]), .nibble_out(sub1_result[15:12]));
-    substitute sbox11 (.nibble_in(data_xor[11: 8]), .nibble_out(sub1_result[11: 8]));
-    substitute sbox12 (.nibble_in(data_xor[ 7: 4]), .nibble_out(sub1_result[ 7: 4]));
-    substitute sbox13 (.nibble_in(data_xor[ 3: 0]), .nibble_out(sub1_result[ 3: 0]));
-
-    wire [15:0] shift1_result;
-    shift_rows shift1 (.data_in(sub1_result), .data_out(shift1_result));
-    wire [15:0] round2_result;
-    assign round2_result = shift1_result ^ rk2;
-
+    wire [15:0] sub2;
+    substitute substitute_2 (.data_in(data_xor), .data_out(sub2));
+    wire [15:0] shift2;
+    shift_rows shift_2 (.data_in(sub2), .data_out(shift2));
+    wire [15:0] round2;
+    assign round2 = shift2 ^ rk2;
 
     always @(posedge clk) begin
-        data_out = round2_result;
+        data_out = round2;
         done = 1;
     end
-
-
-
 endmodule
