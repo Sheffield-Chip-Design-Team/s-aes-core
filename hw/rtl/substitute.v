9d3ef0f1dd2b131f3da2a51531eb0fa16d0acb96
Ralph Greer-Walker <gralp@Ralphs-MacBook-Pro.local>
2026-07-14 03:12:12 +0100

    Fix typo in substitute

--- hw/rtl/substitute.v
+++ hw/rtl/substitute.v
@@ -1,26 +1,26 @@
 module substitute (
-    input  wire [3:0] nibble_in,
-    output reg  [3:0] nibble_out
+    input  wire [3:0] data_in,
+    output reg  [3:0] data_out
 );
     always @(*) begin
-        case (nibble_in)
-            4'h0: nibble_out = 4'h9;
-            4'h1: nibble_out = 4'h4;
-            4'h2: nibble_out = 4'hA;
-            4'h3: nibble_out = 4'hB;
-            4'h4: nibble_out = 4'hD;
-            4'h5: nibble_out = 4'h1;
-            4'h6: nibble_out = 4'h8;
-            4'h7: nibble_out = 4'h5;
-            4'h8: nibble_out = 4'h6;
-            4'h9: nibble_out = 4'h2;
-            4'hA: nibble_out = 4'h0;
-            4'hB: nibble_out = 4'h3;
-            4'hC: nibble_out = 4'hC;
-            4'hD: nibble_out = 4'hE;
-            4'hE: nibble_out = 4'hF;
-            4'hF: nibble_out = 4'h7;
-            default: nibble_out = 4'h0;
+        case (data_in)
+            4'h0: data_out = 4'h9;
+            4'h1: data_out = 4'h4;
+            4'h2: data_out = 4'hA;
+            4'h3: data_out = 4'hB;
+            4'h4: data_out = 4'hD;
+            4'h5: data_out = 4'h1;
+            4'h6: data_out = 4'h8;
+            4'h7: data_out = 4'h5;
+            4'h8: data_out = 4'h6;
+            4'h9: data_out = 4'h2;
+            4'hA: data_out = 4'h0;
+            4'hB: data_out = 4'h3;
+            4'hC: data_out = 4'hC;
+            4'hD: data_out = 4'hE;
+            4'hE: data_out = 4'hF;
+            4'hF: data_out = 4'h7;
+            default: data_out = 4'h0;
         endcase
     end
 endmodule
